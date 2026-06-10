d3c04f977ce7bc142478000d37ea9da60f27d6f9
JayeshKalkate <kalkatejayesh@gmail.com>
2026-06-10 19:19:13 +0530

    fix the templates and urls

--- movieshub/urls.py
+++ movieshub/urls.py
@@ -16,12 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+
     path('', include('coremovieshub.urls')),
-    path("accounts/", include("django.contrib.auth.urls")),
+    
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login", ),
+    
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout", ),
+    
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('coremovieshub.urls')),
+#     path("accounts/", include("django.contrib.auth.urls")),
+# ]
